bbdb89268ebdb485672b184686349e254328f5ef
Rameez Usmani <rameez_usm@hotmail.com>
2020-07-02 14:15:09 +0500

    Examples moved to examples folder

--- examples/transpose.php
+++ examples/transpose.php
@@ -1,13 +1,13 @@
 <?php
 
-include "src/autoload.php";
+include "../src/autoload.php";
 
 use UglyRecommender\VectorsUnequalException;
 use UglyRecommender\NeighborsNotFoundException;
 use UglyRecommender\RecommenderSystem;
 use UglyRecommender\DataHelper\CsvLoadHelper;
 
-$dataMatrix=CsvLoadHelper::load("ratings.csv");
+$dataMatrix=CsvLoadHelper::load("../ratings.csv");
 $recommender=new RecommenderSystem();
 $recommender->setDataMatrix($dataMatrix);
 $recommender->pivotDataMatrix(); //transpose the data matrix*/
